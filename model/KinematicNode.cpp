c3a02fd55adca69500a4dc8c93b90ce28735f9d9
Dominik Muth <dominik@dominik-muth.de>
2016-06-02 19:39:56 +0200

    child management in KinematicNode

--- model/KinematicNode.cpp
+++ model/KinematicNode.cpp
@@ -1,7 +1,9 @@
 #include "KinematicNode.h"
 
-KinematicNode::KinematicNode(MovableSpirit &spirit) : MovableSpirit(spirit)
+KinematicNode::KinematicNode(MovableSpirit &spirit, Point delta, KinematicNode *parent) : MovableSpirit(spirit)
 {
+    this->parent = parent;
+    this->delta = delta;
 }
 
 void KinematicNode::setPos(const Point &pos)
@@ -9,7 +11,7 @@ void KinematicNode::setPos(const Point &pos)
     MovableSpirit::setPos(pos);
     //update children
     for (KinematicNode *node: children)
-        node->setPos(pos + deltas[node].rotated(curAngle));
+        node->setPos(pos + node->delta.rotated(curAngle));
 }
 
 void KinematicNode::translate(const Point delta)
@@ -25,5 +27,43 @@ void KinematicNode::rotate(float angle, CE::Angle type, bool absolut)
     MovableSpirit::rotate(angle, type, absolut);
     //update children
     for (KinematicNode *node : children)
-        node->setPos(pos + deltas[node].rotated(curAngle));
+        node->setPos(pos + node->delta.rotated(curAngle));
+}
+
+void KinematicNode::addChildren(QList<KinematicNode *> nodes)
+{
+    for (KinematicNode *node : nodes)
+        addChild(node);
+}
+
+void KinematicNode::addChild(KinematicNode *node)
+{
+    //return if node is already child of this
+    if (children.contains(node))
+        return;
+
+    //remove as child from other parent if a parent is present
+    if (node->parent)
+        node->parent->removeChild(node);
+
+    //add node as child
+    node->parent = this;
+    children.append(node);
+}
+
+void KinematicNode::setParent(KinematicNode *node)
+{
+    if (node)
+        node->addChild(this);
+    else {
+        if (parent)
+            parent->removeChild(this);
+        parent = NULL;
+    }
+
+}
+
+void KinematicNode::removeChild(KinematicNode *node)
+{
+    children.removeOne(node);
 }
--- model/KinematicNode.h
+++ model/KinematicNode.h
@@ -8,10 +8,19 @@
 #include "model/MovableSpirit.h"
 #include "model/Types.h"
 
+/**
+ * @brief The KinematicNode class represents a node of a kinematic
+ */
 class KinematicNode : public MovableSpirit
 {
 public:
-    KinematicNode(MovableSpirit &spirit);
+    /**
+     * @brief creates a new KinematicNode from a MovableSpirit
+     * @param spirit spirit for the node
+     * @param delta delta position if the parent angle = 0
+     * @param parent optional parent of this node
+     */
+    KinematicNode(MovableSpirit &spirit, Point delta, KinematicNode *parent = NULL);
 
     /**
      * @brief set the position of this spirit
@@ -33,9 +42,34 @@ public:
      */
     void rotate(float angle, CE::Angle type, bool absolut = false);
 
+    /**
+     * @brief add a list of children to this node
+     * @param nodes children to add
+     */
+    void addChildren(QList<KinematicNode*> nodes);
+
+    /**
+     * @brief add a child to this node (use either this or setParent, not both are necessary)
+     * @param node child to add
+     */
+    void addChild(KinematicNode *node);
+
+    /**
+     * @brief setParent sets this node as a child to another node (use either this or addChild, not both are necessary)
+     * @param node node of the new parent
+     */
+    void setParent(KinematicNode *node);
+
+    /**
+     * @brief remove a child
+     * @param node child to remove
+     */
+    void removeChild(KinematicNode *node);
+
 protected:
+    KinematicNode* parent;
+    Point delta;
     QList<KinematicNode*> children;
-    QHash<KinematicNode*, Point> deltas;
 };
 
 #endif // KINEMATICNODE_H
